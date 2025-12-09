53a82fabfc72f83aa30190817a65d49389a8c89c
thaops <127478689+thaops@users.noreply.github.com>
2025-12-09 14:34:03 +0700

    feat: save device id iOS

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -9,6 +9,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   flutter_inappwebview_windows
+  flutter_secure_storage_windows
   flutter_udid
   gal
   permission_handler_windows
